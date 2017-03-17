e625788514a9784a41fcef640f52c04ee619d5f9
Alexandr Ablovatski <ablovatski@gmail.com>
2017-03-17 09:21:23 -0400

    Create gammy_settings.py

---
+++ gammy_settings.py
@@ -0,0 +1,9 @@
+"""
+This config file contains constants describing 
+the specific environment where this script is used.
+"""
+
+# Google Apps domain, e.g. 'university.edu'
+GSUITEDOMAIN = ''
+# Pattern that identifies student user names, e.g. _
+STUPATTERN = '_'
